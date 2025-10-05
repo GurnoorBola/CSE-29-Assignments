a44b56b80aabd508af67b448d635871a323754ef
gurnoor <gsbola@ucsc.edu>
2025-10-04 22:57:08 -0700

    Added is_ascii

--- pa1-utf8/utf8analyzer.c
+++ pa1-utf8/utf8analyzer.c
@@ -1,2 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+
+uint8_t is_ascii(char string[]) {
+	int length = strlen(string);
+	
+	for (int i = 0; i < length; i++){
+		if ((string[i] >> 7) != 0){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+
+
+
+int main(int argc, char** argv) {
+	char buffer[100];
+	uint8_t result;
+
+	printf("Valid ASCII: ");
+	if ((fgets(buffer, 99, stdin) == NULL)){
+		printf("Error");
+		return 1;
+	}
+	result = is_ascii(buffer);
+	printf("%s\n", result ? "true" : "false");
+	return 0;
+}
