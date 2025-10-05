c7f3ffa98612b8c2cfe58532dacfdfeb095e4398
gurnoor <gsbola@ucsc.edu>
2025-10-04 23:44:30 -0700

    Added uppercase, length, and codepoint count functions

--- pa1-utf8/utf8analyzer.c
+++ pa1-utf8/utf8analyzer.c
@@ -3,8 +3,8 @@
 #include <stdint.h>
 #include <string.h>
 
-uint8_t is_ascii(char string[]) {
-	int length = strlen(string);
+uint8_t is_ascii(char string[], size_t l) {
+	size_t length = l; 
 	
 	for (int i = 0; i < length; i++){
 		if ((string[i] >> 7) != 0){
@@ -14,20 +14,60 @@ uint8_t is_ascii(char string[]) {
 	return 1;
 }
 
+int32_t capitalize_ascii(char str[], size_t l) {
+	int32_t res = 0;
+	size_t length = l;
+	for (int i=0; i < length; i++){
+		if (str[i] >= 97 && str[i] <= 122){
+			res += 1;
+			str[i] -= 32;
+		}
+	}
+	return res;
+}
 
+int32_t utf8_strlen(char str[], size_t l) {
+    size_t length = l;
+    int32_t res = 0;
 
-
+    for (int i=0; i < length; i++){
+			unsigned char c = (unsigned char)str[i];
+			if ((c>>6) != 0b10){
+				res += 1;
+			}
+    }
+    return res;
+}
 
 int main(int argc, char** argv) {
 	char buffer[100];
-	uint8_t result;
-
-	printf("Valid ASCII: ");
+	uint8_t valid_ascii;
+	
+	printf("Enter a UTF-8 encoded string: ");
 	if ((fgets(buffer, 99, stdin) == NULL)){
 		printf("Error");
 		return 1;
 	}
-	result = is_ascii(buffer);
-	printf("%s\n", result ? "true" : "false");
+
+	//strip new line
+	size_t length = strcspn(buffer, "\n");
+	if (buffer[length] == '\n'){
+		buffer[length] = '\0';
+	}
+
+
+	valid_ascii = is_ascii(buffer, length);
+	printf("Valid ASCII: %s\n", valid_ascii ? "true" : "false");
+
+	char caps[100];
+	strcpy(caps, buffer);
+	capitalize_ascii(caps, length);
+	printf("Upercased ASCII: %s\n", caps);
+
+	printf("Length in bytes: %ld\n", length);
+	
+	
+	int32_t utf_len = utf8_strlen(buffer,length);
+	printf("Number of code points: %d\n", utf_len);
 	return 0;
 }
